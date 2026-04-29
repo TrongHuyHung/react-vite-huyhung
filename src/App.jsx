2e9db7e3938b207f1276417035399bf1b2b8eefa
Huy Hùng <nthuyhung1307@gmail.com>
2026-04-29 20:18:34 +0700

    Video 29

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+//Update new file
+import MyComponent from './components/learn/MyComponent'
+import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 
-const MyComponent = () => {
-  return (
-    <div>Hello world</div>
-  );
-}
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -24,6 +22,8 @@ const App = () => {
       </div>
       <h1>Hello world & Huy Huy</h1>
       <MyComponent />
+      <SecondComponent />
+      <ThirdComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,12 @@
+const MyComponent = () => {
+    return (
+        <div>Hello world</div>
+    );
+}
+const FakeComponent = () => {
+    return (
+        <div>Fake Hello world</div>
+    );
+}
+
+export default MyComponent;
