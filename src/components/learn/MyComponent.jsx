0a8189d52028278db3fdb8c05bafff3d6c7321e1
Huy Hùng <nthuyhung1307@gmail.com>
2026-04-30 14:05:58 +0700

    Video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,16 @@
 //Fragment
 import './style.css'
 const MyComponent = () => {
+    // const name = "Huy Hung"; //String
+    // const name = [1, 2, 3]; //Array
+    // const name = true; //String
+    const name = { name: "Hi", age: 26 }; //object
+    // const name = "Huy Hung"; //String
+    // const name = 25; //Number
     return (
         <>
-            <div>Hello world</div>
+            <div>{JSON.stringify(name)} && </div>
+            <div>{console.log("Hi")}</div>
             <div className="child">Child</div>
         </>
     );
