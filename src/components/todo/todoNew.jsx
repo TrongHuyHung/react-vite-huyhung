e53ef0125ab71868ff093772796f74ed718d379b
Huy Hùng <nthuyhung1307@gmail.com>
2026-05-01 13:33:25 +0700

    Video 36

--- src/components/todo/todoNew.jsx
+++ src/components/todo/todoNew.jsx
@@ -3,10 +3,20 @@ const TodoNew = (props) => {
     const { addNewTodo } = props;
 
     //addNewTodo("1");
+    const handleClick = () => {
+        alert("click me");
+    }
+    const handOnChange = (event) => {
+        console.log("==>", event);
+    }
     return (
         <div className='todo-input'>
-            <input type="text" placeholder='Enter your name' />
-            <button >Add</button>
+            <input type="text" placeholder='Enter your name'
+                onChange={(event) => handOnChange(event.target.value)}
+            />
+            <button onClick={handleClick}
+            >Add
+            </button>
         </div>
     );
 }
